8ad3b6a8240586cc67a90445e995a0e7aaf13382
OneYX <504887284@qq.com>
2021-04-17 15:56:24 +0800

    format

--- docker/shell_script_mod.sh
+++ docker/shell_script_mod.sh
@@ -1,19 +1,16 @@
 #！/bin/sh
 
-echo "mergedListFile: $mergedListFile"
-
 mergedListFile="/scripts/docker/merged_list_file.sh"
 
 echo "设定脚本中的代理认证..."
 sed -i '/port: process.env.TG_PROXY_PORT \* 1$/s/$/,\proxyAuth: process.env.TG_PROXY_AUTH/' /scripts/*.js
 
-echo "下载脚本并追加定时任务..."
-
+echo "下载脚本..."
 downloadFiles="jdJxncTokens jx_cfdtx"
-
 ROOT_PATH="https://waxgourd.coding.net/p/github/d/MyActions/git/raw/main"
 for fileName in $downloadFiles; do
     curl -Lso- $ROOT_PATH/$fileName.js > /scripts/$fileName.js
 done
 
+echo "追加定时任务..."
 curl -Lso- https://waxgourd.coding.net/p/github/d/jd_scripts/git/raw/develop/docker/my_crontab_list.sh >> $mergedListFile
