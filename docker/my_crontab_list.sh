d5cf3f0d8dec6f7328515c1e95b21a9a1aac924a
yipingdong <pingdongyi@gmail.com>
2021-05-10 15:37:42 +0800

    add cron rule

--- docker/my_crontab_list.sh
+++ docker/my_crontab_list.sh
@@ -1,1 +1,3 @@
 59 23 * * * sleep 55; node /scripts/jx_cfdtx.js >> /scripts/logs/jx_cfdtx.log 2>&1
+1 0-23/1 * * * node /scripts/jd_super_redrain.js >> /scripts/logs/jd_super_redrain.log 2>&1
+30 20-23/1 * * * node /scripts/jd_half_redrain.js >> /scripts/logs/jd_half_redrain.log 2>&1
