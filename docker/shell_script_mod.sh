fda32182e5cfac13840c1417c3127f3286e4ac45
yipingdong <pingdongyi@gmail.com>
2021-05-10 15:33:40 +0800

    add redrain

--- docker/shell_script_mod.sh
+++ docker/shell_script_mod.sh
@@ -1,16 +1,19 @@
 #！/bin/sh
 
+function downloadFiles() {
+  for fileName in $2; do
+      curl -Lso- $1/$fileName.js > /scripts/$fileName.js
+  done
+}
+
 mergedListFile="/scripts/docker/merged_list_file.sh"
 
 echo "设定脚本中的代理认证..."
 sed -i '/port: process.env.TG_PROXY_PORT \* 1$/s/$/,\proxyAuth: process.env.TG_PROXY_AUTH/' /scripts/*.js
 
 echo "下载脚本..."
-downloadFiles="jdJxncTokens jx_cfdtx"
-ROOT_PATH="https://waxgourd.coding.net/p/github/d/MyActions/git/raw/main"
-for fileName in $downloadFiles; do
-    curl -Lso- $ROOT_PATH/$fileName.js > /scripts/$fileName.js
-done
+downloadFiles "https://waxgourd.coding.net/p/github/d/MyActions/git/raw/main" "jdJxncTokens jx_cfdtx"
+downloadFiles "https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx" "jd_half_redrain jd_super_redrain"
 
 echo "追加定时任务..."
 curl -Lso- https://waxgourd.coding.net/p/github/d/jd_scripts/git/raw/develop/docker/my_crontab_list.sh >> $mergedListFile
